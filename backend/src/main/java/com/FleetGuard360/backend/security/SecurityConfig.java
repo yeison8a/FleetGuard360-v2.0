e3e0aca6e4a3523561eb180bd290073a33ea6fc5
yeison8a35 <yeison.ochoa@udea.edu.co>
2025-06-14 20:50:27 -0500

    cors /

--- backend/src/main/java/com/FleetGuard360/backend/security/SecurityConfig.java
+++ backend/src/main/java/com/FleetGuard360/backend/security/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("https://fleet-guard360-v1-0-1a7g.vercel.app/"));
+        configuration.setAllowedOrigins(List.of("https://fleet-guard360-v1-0-1a7g.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
